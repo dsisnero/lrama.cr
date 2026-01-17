2a37756f7c2a03e617fc41d6e99e8b63ea7ba65a
dsisnero <dsisnero@gmail.com>
2026-01-17 13:27:28 -0700

    Parse code and param directives
    Handle %union, %code, %printer/%destructor/%error-token, and param blocks
    
    Capture lex/parse params and initial action in Grammar with spec coverage

--- src/lrama/grammar.cr
+++ src/lrama/grammar.cr
@@ -14,6 +14,14 @@ module Lrama
     getter nterm_declarations : Array(SymbolGroup)
     getter precedence_declarations : Array(PrecedenceDeclaration)
     property start_symbol : String?
+    property union_code : Lexer::Token::UserCode?
+    property lex_param : String?
+    property parse_param : String?
+    property initial_action : Lexer::Token::UserCode?
+    getter percent_codes : Array(PercentCode)
+    getter printers : Array(CodeDeclaration)
+    getter destructors : Array(CodeDeclaration)
+    getter error_tokens : Array(CodeDeclaration)
 
     def initialize
       @declarations_tokens = [] of Lexer::TokenValue
@@ -30,6 +38,14 @@ module Lrama
       @nterm_declarations = [] of SymbolGroup
       @precedence_declarations = [] of PrecedenceDeclaration
       @start_symbol = nil
+      @union_code = nil
+      @lex_param = nil
+      @parse_param = nil
+      @initial_action = nil
+      @percent_codes = [] of PercentCode
+      @printers = [] of CodeDeclaration
+      @destructors = [] of CodeDeclaration
+      @error_tokens = [] of CodeDeclaration
     end
 
     def tokens_for(section : Symbol)
@@ -74,6 +90,22 @@ module Lrama
       getter tokens : Array(Lexer::Token::Base)
 
       def initialize(@kind : Symbol, @tag : Lexer::Token::Tag?, @tokens : Array(Lexer::Token::Base))
+      end
+    end
+
+    struct PercentCode
+      getter id : String
+      getter code : Lexer::Token::UserCode
+
+      def initialize(@id : String, @code : Lexer::Token::UserCode)
+      end
+    end
+
+    struct CodeDeclaration
+      getter targets : Array(Lexer::Token::Base)
+      getter code : Lexer::Token::UserCode
+
+      def initialize(@targets : Array(Lexer::Token::Base), @code : Lexer::Token::UserCode)
       end
     end
   end
