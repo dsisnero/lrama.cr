ca282e5a6f88983f9b57effd0a927e30be494553
dsisnero <dsisnero@gmail.com>
2026-01-17 13:02:10 -0700

    Add user code reference parsing
    Implement Grammar::Reference and UserCode reference scanning
    
    Add specs covering 15813, , $<tag>, and @ references

--- src/lrama.cr
+++ src/lrama.cr
@@ -2,6 +2,7 @@ require "./lrama/version"
 require "./lrama/cli"
 require "./lrama/lexer"
 require "./lrama/grammar"
+require "./lrama/grammar/reference"
 require "./lrama/grammar_parser"
 
 module Lrama
