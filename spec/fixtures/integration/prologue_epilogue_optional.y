3285ad59454fa1ec62c52260b52c9c7d39c6219d
Dominic Sisneros <dominic@Mac.lan>
2026-01-17 21:44:44 -0700

    Add parser fixtures for parameterized rules
    Port Ruby lrama fixtures for parameterized rules and prologue/epilogue coverage into spec fixtures. Add a new grammar parser spec that validates named references, tag handling in rules, and prologue/epilogue capture against these fixtures, and close lrama-7l5.

---
+++ spec/fixtures/integration/prologue_epilogue_optional.y
@@ -0,0 +1,39 @@
+%{
+
+#include <stdio.h>
+#include "prologue_epilogue_optional.h"
+#include "prologue_epilogue_optional-lexer.h"
+
+static int yyerror(YYLTYPE *loc, const char *str);
+
+%}
+
+%union {
+    int val;
+}
+
+%locations
+
+%%
+
+program : /* empty */
+        ;
+
+%%
+
+static int yyerror(YYLTYPE *loc, const char *str) {
+    fprintf(stderr, "parse error: %s\n", str);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 2) {
+        yy_scan_string(argv[1]);
+    }
+
+    if (yyparse()) {
+        fprintf(stderr, "syntax error\n");
+        return 1;
+    }
+    return 0;
+}
---
+++ spec/grammar_parser_fixtures_spec.cr
@@ -0,0 +1,49 @@
+require "./spec_helper"
+
+private def parse_fixture(path : String)
+  grammar_file = Lrama::Lexer::GrammarFile.new(path, File.read(path))
+  lexer = Lrama::Lexer.new(grammar_file)
+  parser = Lrama::GrammarParser.new(lexer)
+  parser.parse
+end
+
+describe Lrama::GrammarParser do
+  it "parses parameterized rule fixture with named references" do
+    path = File.join(__DIR__, "fixtures", "parameterized", "user_defined", "with_action_and_named_references.y")
+    grammar = parse_fixture(path)
+
+    grammar.parameterized_rules.size.should eq 1
+    rule = grammar.parameterized_rules.first
+    rule.name.should eq "sum"
+    rule.parameters.map(&.s_value).should eq ["X", "Y"]
+    rule.tag.should_not be_nil
+    rule.tag.try(&.s_value).should eq "<i>"
+
+    rhs = rule.rhs_list.first
+    rhs.symbols.size.should eq 3
+    rhs.symbols.first.alias_name.should eq "summand"
+    rhs.symbols.last.alias_name.should eq "addend"
+  end
+
+  it "parses parameterized rule tags in rules section" do
+    path = File.join(__DIR__, "fixtures", "parameterized", "user_defined", "with_tag.y")
+    grammar = parse_fixture(path)
+
+    rules = grammar.rule_builders.select { |builder| builder.lhs.try(&.s_value) == "program" }
+    rules.size.should eq 2
+    tagged = rules.find do |builder|
+      builder.rhs.first?.is_a?(Lrama::Lexer::Token::InstantiateRule) &&
+        builder.rhs.first.as(Lrama::Lexer::Token::InstantiateRule).lhs_tag.try(&.s_value) == "<s>"
+    end
+    tagged.should_not be_nil
+  end
+
+  it "captures prologue and epilogue in integration fixture" do
+    path = File.join(__DIR__, "fixtures", "integration", "prologue_epilogue_optional.y")
+    grammar = parse_fixture(path)
+
+    grammar.prologue.should_not be_nil
+    grammar.prologue_first_lineno.should eq 1
+    grammar.epilogue.should_not be_nil
+  end
+end
